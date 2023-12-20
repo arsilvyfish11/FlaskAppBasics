24cc6370454348830ec223c58ae8202f4765f683
arsilvyfish11 <aryan2601@kgpian.iitkgp.ac.in>
2023-12-20 17:34:52 +0530

    All the files for the App

---
+++ TestingReact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class TestingReact extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+                HELLO WORLD!!
+            </div>
+        );
+    }
+}
+
+// have to render it from root
